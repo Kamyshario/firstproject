481ac392b49822da7cab3ca43a88f448b8e8a95e
Kamyshario <146371570+Kamyshario@users.noreply.github.com>
2024-09-17 15:22:27 +0300

    Add files via upload

---
+++ Skills.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function () {
+    output.innerHTML = 'На Берлин! Ой, то есть на главную!';
+})
